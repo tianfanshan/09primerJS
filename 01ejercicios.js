3fcb36fc38bd301cdcceb86a75c200c862aefda4
tianfanshan <stf30111994@gmail.com>
2022-04-11 15:06:34 +0200

    primer JS01

---
+++ 01ejercicios.js
@@ -0,0 +1,165 @@
+//Variables
+alert("JavaScript funcionando correctamente");
+let variableSinValor;
+let uno=true;
+console.log(uno);
+let dos=false;
+console.log(dos);
+const PI=3.14;
+console.log(PI);
+const TAU=(PI*2);
+console.log(TAU);
+const miNombre="Tianfan Shan";
+console.log("Mi nombre: ", miNombre);
+const miNumeroFav=9;
+const tres=true;
+console.log(miNumeroFav);
+console.log(miNombre.length);
+console.log(typeof miNumeroFav);
+console.log("Mi nombre es"+miNombre+"y mi numero favorito es"+miNumeroFav);
+const cuatro="Seré un crack en JavaScript al terminar el bootcamp";
+    let val;
+        val=cuatro.toUpperCase();
+        console.log(val);
+const cinco="Hola soy un crack";
+    let val1;
+        val1=cinco.substring(0,5);
+        console.log(val1);
+console.log(typeof miNumeroFav)
+const miVariableCambiada = miNumeroFav.toString()
+console.log(typeof miVariableCambiada)
+console.log("Me has cambiado el tipo?: ", miVariableCambiada)
+//var seis=
+
+
+//Arrays
+const arrayVacio =[];
+console.log(arrayVacio);
+
+const arrayNumeros=[0,1,2,3,4,5,6,7,8,9];
+console.log(arrayNumeros);
+
+const arrayNumeros2=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+console.log(arrayNumeros2);
+
+const arrayNumerosPares=[0,2,4,6,8];
+console.log(arrayNumerosPares);
+
+const arrayNumerosNeg=[0,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+console.log(arrayNumerosNeg);
+
+const holaMundo=['Hola','Mundo'];
+console.log(holaMundo);
+
+const loGuardoTodo=['hola','que',23,42.33,'tal'];
+console.log(loGuardoTodo);
+
+const arrayDeArrays=[[756,'nombre'],[225,'apellido'][298,'direccion']];
+console.log(loGuardoTodo.length);
+
+const siete=[1,2,3,4];
+    siete.push("loGuardoTodo");
+    console.log(siete);
+
+//
+
+//
+
+console.log(loGuardoTodo[1]);
+
+loGuardoTodo.push("Euralio");
+console.log(loGuardoTodo);
+
+//Objetos
+const coche={
+    marca:"BMW",
+    modelo:"seie1",
+    matricula:"123bcv",
+};
+console.log(coche);
+
+const casa={
+    codPostal:46790,
+    calle:"Jesus",
+    portal:4,
+    piso:6,
+}
+console.log(casa);
+
+const FullStackDeveloper={
+    lenguajes:['javascript','php'],
+    proyectos:['mi pagina personal'],
+}
+console.log(FullStackDeveloper);
+
+const Perro={
+    nombre:"Yago",
+    raza:"aleman",
+    color:"negro",
+    edad:5,
+}
+console.log(Perro);
+
+const Noticia={
+    titular:"no se que",
+    curpo:"rellenar",
+}
+console.log(Noticia);
+
+const Persona={
+    nombre:"Juan",
+    apeido:"Frutero",
+    edad:15,
+}
+console.log(Persona);
+
+console.log(Persona.nombre);
+
+console.log(FullStackDeveloper.lenguajes);
+
+const Portatil={
+    marca1:"Hp",
+    marca2:"Asus",
+    marca3:"LG",
+}
+console.log(Portatil);
+
+const marca=["HP","ASUS","LG"];
+console.log(marca);
+
+const Led={
+    rojo:["Rojo"],
+    verde:["Verde"],
+    azul:["Azul"],
+}
+console.log(Led);
+
+const O_Error={
+
+}
+
+const Grup={
+
+}
+
+const impresora={
+    tinta:{
+        rojo:["lleno"],
+        verde:["poco"],
+        azul:["medio"],
+    }
+}
+console.log(impresora.tinta.rojo);
+
+const Movil={
+    especificaciones:["especificaciones"],
+}
+console.log(Movil.especificaciones);
+
+console.log(Portatil.marca1);
+Portatil.marca1 = "Msi";
+console.log(Portatil.marca1);
+
+//
+//
+//
